5aa83a3a90d503439b78ef56e45dd5bebb2a4b08
Anikesh994 <anikeshkumar994@gmail.com>
2026-04-19 14:46:07 +0530

    Time: 0 ms (100.00%) | Memory: 30.7 MB (69.80%) - LeetSync

---
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        vector<int>ma;
+        int n =nums.size();
+        int mi1 =INT_MAX;
+        int ma1 = INT_MIN;
+        for(int i=0;i<n;i++){
+            ma1 = max(ma1,nums[i]);
+            
+            ma.push_back(ma1);
+
+        }
+        int ans = INT_MAX;
+        for(int i=n-1;i>=0;i--){
+            mi1 = min(mi1,nums[i]);
+            if(ma[i]-mi1 <= k){
+                ans=min(ans,i);
+            }
+        }
+        if(ans==INT_MAX) return -1;
+        return ans;       
+    }
+};
