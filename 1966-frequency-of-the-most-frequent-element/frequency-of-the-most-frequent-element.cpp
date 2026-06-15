b26861391ec732317363f9ba41480147235aee29
Anikesh994 <anikeshkumar994@gmail.com>
2026-06-15 20:43:23 +0530

    Time: 43 ms (40.61%) | Memory: 105.9 MB (33.09%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,35 +1,22 @@
 class Solution {
-
-int fn(int low,int high ,vector<int>& nums ,vector<long long>& p,int k){
-    int l =low;
-    int h =high;
-    while(low<=high){
-        int mid =(low +high)/2;
-        if((1LL*(h-mid+1)*nums[h])-(p[h+1]-p[mid]) <=k){
-            high=mid-1;
-        }
-        else{
-            low=mid+1;
-        }
-    }
-    return h-low+1;
-}
-
-
 public:
     int maxFrequency(vector<int>& nums, int k) {
-        int n =nums.size();
-        sort(nums.begin(),nums.end());
-        vector<long long>pre(n+1,0);
-        for(int i =1;i<=n;i++){
-            pre[i]=pre[i-1]+nums[i-1];
+        sort(nums.begin(), nums.end());
+
+        long long sum = 0;
+        int l = 0;
+        int ans = 1;
+
+        for (int r = 0; r < nums.size(); r++) {
+            sum += nums[r];
+
+            while (1LL * nums[r] * (r - l + 1) - sum > k) {
+                sum -= nums[l++];
+            }
+
+            ans = max(ans, r - l + 1);
         }
-        int ans=1;
-        for(int i=n-1;i>=0;i--){
-            ans=max(ans,fn(0,i,nums,pre,k));
-            if(i<=ans) break;
-        }
-        
-        return ans;                                   
+
+        return ans;
     }
 };
