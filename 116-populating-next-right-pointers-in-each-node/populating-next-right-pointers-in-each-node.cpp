d77899fd8a1637d0c5e73a6a01f4a145cdc614e9
Anikesh994 <anikeshkumar994@gmail.com>
2026-06-26 17:07:22 +0530

    Time: 14 ms (54.11%) | Memory: 18.9 MB (80.99%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -1,41 +1,30 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
-
 class Solution {
-
-void solve(Node * root){
-    if(root == nullptr || root->left == nullptr) return ;
-
-    root->left->next = root->right;
-    if(root->next != nullptr){
-        root->right->next =root->next->left;
-    }
-
-    solve(root->left);
-    solve(root->right);
-}
-
-
-
-
 public:
     Node* connect(Node* root) {
-        solve(root);
+        if (root == nullptr)
+            return nullptr;
+
+        Node* leftmost = root;
+
+        while (leftmost->left) {
+            Node* curr = leftmost;
+
+            while (curr) {
+                // Connect left child to right child
+                curr->left->next = curr->right;
+
+                // Connect right child to next node's left child
+                if (curr->next) {
+                    curr->right->next = curr->next->left;
+                }
+
+                curr = curr->next;
+            }
+
+            // Move to next level
+            leftmost = leftmost->left;
+        }
+
         return root;
     }
 };
