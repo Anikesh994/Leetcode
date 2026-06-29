0d7019a9768c5f6e341af8652c890cdb00a816ae
Anikesh994 <anikeshkumar994@gmail.com>
2026-06-29 19:39:37 +0530

    Time: 3 ms (18.93%) | Memory: 12.7 MB (10.92%) - LeetSync

--- 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     int numOfStrings(vector<string>& patterns, string word) {
         int n =word.size();
-        unordered_map<string ,int>um;
+        unordered_map<string,int>um;
+        for(int i=0;i<patterns.size();i++){
+            um[patterns[i]]++;
+        }
+        int ans=0;
         for(int i=0;i<n;i++){
             string s="";
             for(int j=i;j<n;j++){
                 s+=word[j];
-                um[s]++;
+                if(um.find(s)!=um.end()){
+                    ans+=um[s]; 
+                    um.erase(s);
+                    //if(um[s]==0) um.erase(s);
+                } 
             }
             
         }
-        int ans=0;
-        for(int i=0;i<patterns.size();i++){
-            if(um.find(patterns[i])!=um.end()) ans++;
-        }
+        
+        
         return ans;
     }
 };
