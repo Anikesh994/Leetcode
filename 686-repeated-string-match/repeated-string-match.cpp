878759af490cd5f1e676ffe45ac119997828f132
Anikesh994 <anikeshkumar994@gmail.com>
2026-06-29 22:29:39 +0530

    Time: 246 ms (8.55%) | Memory: 9.5 MB (94.52%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -3,16 +3,34 @@ public:
     int repeatedStringMatch(string a, string b) {
         int n1=a.size();
         int n2=b.size();
-        int ans=1;
-        string q1=a;
-        while(a.size()<=1e4){
-            if(a.find(b)!=-1) return ans;
-            else{
-                ans++;
-                a+=q1;
-
+        int a1=n2/n1;
+        char first =b[0];
+        int q=-1;
+        vector<int>temp;
+        for(int i=0;i<n1;i++){
+            if(a[i]==first){
+                temp.push_back(i);
             }
         }
+        if(temp.size()==0) return -1;
+        int ans=1;
+        for(int i=0;i<temp.size();i++){
+            ans=1;
+            int j=temp[i];
+            int q=0;
+            while(q<n2 && b[q]==a[j]){
+                q++;
+                j++;
+                if(j==n1){
+                    
+                    j=0;
+                    if(q<n2) ans++;
+                }
+                
+            }
+            if(q==n2) return ans;
+        }
         return -1;
+
     }
 };
