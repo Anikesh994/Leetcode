c6ac19646ae144167e53e025c38f8b06cf79ef8c
Anikesh994 <anikeshkumar994@gmail.com>
2026-06-29 22:38:25 +0530

    Time: 7 ms (44.60%) | Memory: 9.7 MB (66.58%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.cpp
+++ 686-repeated-string-match/repeated-string-match.cpp
@@ -1,36 +1,22 @@
+#include <string>
+
 class Solution {
 public:
-    int repeatedStringMatch(string a, string b) {
-        int n1=a.size();
-        int n2=b.size();
-        int a1=n2/n1;
-        char first =b[0];
-        int q=-1;
-        vector<int>temp;
-        for(int i=0;i<n1;i++){
-            if(a[i]==first){
-                temp.push_back(i);
-            }
+    int repeatedStringMatch(std::string a, std::string b) {
+        std::string repeatA = a;
+        int count = 1;
+        if(a.find(b)!=-1) return 1;
+        while (repeatA.length() < b.length()) {
+            repeatA += a;
+            count++;
+            if (repeatA.find(b) != std::string::npos) return count;
         }
-        if(temp.size()==0) return -1;
-        int ans=1;
-        for(int i=0;i<temp.size();i++){
-            ans=1;
-            int j=temp[i];
-            int q=0;
-            while(q<n2 && b[q]==a[j]){
-                q++;
-                j++;
-                if(j==n1){
-                    
-                    j=0;
-                    if(q<n2) ans++;
-                }
-                
-            }
-            if(q==n2) return ans;
-        }
-        return -1;
 
+       
+        repeatA += a;
+        count++;
+        if (repeatA.find(b) != std::string::npos) return count;
+
+        return -1;
     }
 };
