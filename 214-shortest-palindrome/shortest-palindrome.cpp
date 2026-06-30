aa8cdb23421f06dfb90bb21cac7d6d1bca2f9da6
Anikesh994 <anikeshkumar994@gmail.com>
2026-06-30 10:41:17 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (94.56%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        int n = s.size();
-        vector<int>presum(2*n+1,0);
-        string s2=s;
-        reverse(s2.begin(),s2.end());
-        string s1 = s + '#' + s2;
-        int i=1;
-        int length=0;
-        int ans=0;
-        while(i<s1.size()){
-            if(s1[i]==s1[length]){
-                presum[i]=length+1;
-                length++;
-                ans=max(ans,length);
-                i++;
-            }
-            else{
-                if(length!=0){
-                    length =presum[length-1];
-                }
-                else{
-                    presum[i]=0;
-                    i++;
-                }
-            }
-        }
-        for(int i=0;i<(s.size()-length);i++){
-            s2.push_back(s[length+i]);
-        }
-        reverse(s2.begin(),s2.end());
-        return s2;
+       long long base = 131;
+        long long mod = 1e9 + 7;
 
+        long long forward = 0;
+        long long backward = 0;
+        long long power = 1;
+
+        int length = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            int val = s[i];
+
+            // hash of s[0...i]
+            forward = (forward * base + val) % mod;
+
+            // hash of reverse(s[0...i])
+            backward = (backward + val * power) % mod;
+
+            if (forward == backward) {
+                length = i + 1;
+            }
+
+            power = (power * base) % mod;
+        }
+
+        string s2 = s.substr(length);
+        reverse(s2.begin(), s2.end());
+
+        return s2 + s;
     }
 };
