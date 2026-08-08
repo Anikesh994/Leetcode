f8adf9f9144c4fee14d100dff190eb9b16e43c13
Anikesh994 <anikeshkumar994@gmail.com>
2026-08-08 18:41:28 +0530

    Time: 16 ms (19.66%) | Memory: 55.9 MB (59.41%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -1,80 +1,89 @@
+
 class Twitter {
-    class cmp {
-    public:
-        bool operator()(const pair<int, int>& a,
-                        const pair<int, int>& b) {
-            return a.first > b.first;   // min-heap based on timestamp
-        }
-    };
+
+class cmp{
+public:
+    bool operator()(const pair<int,int>& a , const pair<int,int>& b){
+        return a.first>b.first;
+    }
+      
+};
 
 public:
-    int c = 0;
+    int c=0;
+    unordered_map<int,unordered_map<int,int>>t1;
+    unordered_map<int,vector<pair<int,int>>>t2;
 
-    // t1[follower][followee]
-    unordered_map<int, unordered_map<int, int>> t1;
-
-    // t2[user] = {timestamp, tweetId}
-    unordered_map<int, vector<pair<int, int>>> t2;
-
-    Twitter() {}
-
+    Twitter() {
+        
+    }
+    
     void postTweet(int userId, int tweetId) {
-        t2[userId].push_back({c, tweetId});
+        t2[userId].push_back({c,tweetId});
         c++;
     }
-
+    
     vector<int> getNewsFeed(int userId) {
-        priority_queue<
-            pair<int, int>,
-            vector<pair<int, int>>,
-            cmp
-        > pq;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,cmp>pq;
 
-        // Include user's own tweets
-        for (auto tweet : t2[userId]) {
-            if (pq.size() < 10) {
-                pq.push(tweet);
-            } else if (pq.top().first < tweet.first) {
-                pq.pop();
-                pq.push(tweet);
+        // own tweets
+        int x = 0;
+        for(auto it1 = t2[userId].rbegin();
+            it1 != t2[userId].rend() && x < 10;
+            it1++, x++){
+
+            if(pq.size()<10){
+                pq.push(*it1);
             }
-        }
-
-        // Include tweets of people userId follows
-        for (auto it : t1[userId]) {
-            int followeeId = it.first;
-
-            for (auto tweet : t2[followeeId]) {
-                if (pq.size() < 10) {
-                    pq.push(tweet);
-                } else if (pq.top().first < tweet.first) {
+            else{
+                auto it3 = pq.top();
+                if(it3.first < it1->first){
                     pq.pop();
-                    pq.push(tweet);
+                    pq.push(*it1);
                 }
             }
         }
 
-        // pq is oldest -> newest because it's a min-heap
-        vector<int> ans;
+        // friends' tweets
+        for(auto it : t1[userId]){
+            
+            x = 0;
 
-        while (!pq.empty()) {
-            ans.push_back(pq.top().second);
+            for(auto it1 = t2[it.first].rbegin(); 
+                it1 != t2[it.first].rend() && x < 10; 
+                it1++, x++){
+
+                if(pq.size()<10){
+                    pq.push(*it1);
+                }
+                else{
+                    auto it3 = pq.top();
+                    if(it3.first < it1->first){
+                        pq.pop();
+                        pq.push(*it1);
+                    }
+                }
+            }
+        }
+
+        vector<int>temp;
+
+        while(!pq.empty()){
+            temp.push_back(pq.top().second);
             pq.pop();
         }
 
-        reverse(ans.begin(), ans.end());
+        reverse(temp.begin(),temp.end());
 
-        return ans;
+        return temp;
     }
-
+    
     void follow(int followerId, int followeeId) {
-        if (followerId == followeeId)
-            return;
-
-        t1[followerId][followeeId] = 1;
+        t1[followerId][followeeId]++;
     }
-
+    
     void unfollow(int followerId, int followeeId) {
         t1[followerId].erase(followeeId);
     }
 };
+
